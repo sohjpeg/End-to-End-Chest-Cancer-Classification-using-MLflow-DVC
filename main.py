e20dc577c24c0fee06b4f0b18da5647b2d6732bd
sohjpeg <sbukhari.bscs23seecs@student.nust.edu.pk>
2025-07-09 17:39:09 +0500

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+# filepath: c:\Users\sohai\OneDrive - National University of Sciences & Technology\Desktop\proj\End-to-End-Chest-Cancer-Classification-using-MLflow-DVC\main.py
 from cnnClassifier import logger
-logger.info("Welcome to CNN Classifier")
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:  # ← Fixed indentation
+    logger.exception(e)
+    raise e
